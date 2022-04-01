91bf9cd213cc8e4c89692f43fae6dd0bba1c1df9
ssghost <ssprof0@gmail.com>
2022-04-02 01:16:28 +0800

    add usedapp.tsx

---
+++ frontend/src/components/Main.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable spaced-comment */
+/// <reference types="react-scripts" />
+import { useEthers } from "@usedapp/core"
+import helperConfig from "../helper-config.json"
+import networkMapping from "../chain-info/deployments/map.json"
+import { constants } from "ethers"
+import brownieConfig from "../brownie-config.json"
+import shit from "../shit.png"
+import eth from "../eth.png"
+import dai from "../dai.png"
+import { YourWallet } from "./yourWallet"
+import { makeStyles } from "@material-ui/core"
+
+export type Token = {
+    image: string
+    address: string
+    name: string
+}
+
+const useStyles = makeStyles((theme: any) => ({
+    title: {
+        color: theme.palette.common.white,
+        textAlign: "center",
+        padding: theme.spacing(4)
+    }
+}))
+
+export const Main = () => {
+    const classes = useStyles()
+    const { chainId, error } = useEthers()
+    const networkName = chainId ? helperConfig[chainId] : "dev"
+    let stringChainId = String(chainId)
+    const shitTokenAddress = chainId ? networkMapping[stringChainId]["ShitToken"][0] : constants.AddressZero
+    const wethTokenAddress = chainId ? brownieConfig["networks"][networkName]["weth_token"] : constants.AddressZero 
+    const fauTokenAddress = chainId ? brownieConfig["networks"][networkName]["fau_token"] : constants.AddressZero
+
+    const supportedTokens: Array<Token> = [
+        {
+            image: shit,
+            address: shitTokenAddress,
+            name: "SHIT"
+        },
+        {
+            image: eth,
+            address: wethTokenAddress,
+            name: "WETH"
+        },
+        {
+            image: dai,
+            address: fauTokenAddress,
+            name: "DAI"
+        }
+    ]
+
+    return (<>
+        <h2 className={classes.title}>Token Farm App</h2>
+        <YourWallet supportedTokens={supportedTokens} />
+    </>)
+}
